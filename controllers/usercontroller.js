0c29420036537d8ef54a6edf82cbd1e68e72261d
Corynne Moody <cmoody@elevenfifty.org>
2021-12-28 16:56:35 -0500

    I think I fixed the bug, I used the bcrypt package instead of bcryptjs

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { Router } = require('express');
 const { User } = require('../models');
